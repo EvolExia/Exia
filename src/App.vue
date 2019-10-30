00a73ead4158afae9bbee314ff529b724d83f6a8
EvolExia <57168300+EvolExia@users.noreply.github.com>
2019-10-30 15:31:32 +0800

    this is one

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+
+    <HelloWorld/>
+    <k/>
+    <shui/>
+    <list/>
+    <cart/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import k from './components/k'
+import shui from './components/shui'
+import list from "./components/list.vue"
+import cart from "./components/cart.vue"
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    k,
+    shui,
+    list,
+    cart
+  }
+}
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="item,index in list">{{item}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
